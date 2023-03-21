ebb58bf8830de9957af493e4b774892d4297d8aa
Alina <An10_23@hotmail.com>
2023-03-20 22:05:04 -0400

    Add CreateSurvey files

--- app/src/main/java/com/example/project_comp4200/CreateSurvey.java
+++ app/src/main/java/com/example/project_comp4200/CreateSurvey.java
@@ -3,12 +3,32 @@ package com.example.project_comp4200;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateSurvey extends AppCompatActivity {
+
+    EditText surveyTitle;
+    EditText surveyDesc;
+
+    EditText question;
+    Button buttonAddQuestion;
+
+    Button buttonPostSurvey;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_survey);
+
+        surveyTitle = findViewById(R.id.surveyTitleEditText);
+        surveyDesc = findViewById(R.id.surveyDescEditText);
+        question = findViewById(R.id.questionEditText);
+        buttonAddQuestion = findViewById(R.id.addQuestionButton);
+        buttonPostSurvey = findViewById(R.id.postSurveyButton);
+
+
+
     }
 }
